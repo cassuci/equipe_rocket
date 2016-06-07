72a66b1961c3219c266f41e5f593f93a7d542e55
cassuci <gabrielcassuci@gmail.com>
2016-06-07 16:07:50 -0300

    Testando

---
+++ app/src/main/java/com/draft/rckt/equiperocket/Database/DatabaseContract.java
@@ -0,0 +1,43 @@
+package com.draft.rckt.equiperocket.Database;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Gabriel on 6/5/2016.
+ */
+public final class DatabaseContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public DatabaseContract() {}
+
+    /* Inner class that defines the table contents */
+    public static abstract class UserEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Users";
+        public static final String COLUMN_NAME_USER_ID = "username";
+        public static final String COLUMN_NAME_PASSWORD = "senha";
+        public static final String COLUMN_NAME_NAME = "nome";
+    }
+
+
+    public static abstract class ReceitaEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Receitas";
+        public static final String COLUMN_NAME_ENTRY_ID = "receita_id";
+        public static final String COLUMN_NAME_USER_ID = "user_id";
+        public static final String COLUMN_NAME_TITLE = "titulo";
+        public static final String COLUMN_NAME_CONTENT = "descricao";
+        public static final String COLUMN_NAME_VALUE = "valor";
+        public static final String COLUMN_NAME_TYPE = "tipo";
+        public static final String COLUMN_NAME_DATE = "data";
+    }
+
+    public static abstract class GastoEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Gastos";
+        public static final String COLUMN_NAME_ENTRY_ID = "gasto_id";
+        public static final String COLUMN_NAME_USER_ID = "user_id";
+        public static final String COLUMN_NAME_TITLE = "titulo";
+        public static final String COLUMN_NAME_CONTENT = "descricao";
+        public static final String COLUMN_NAME_VALUE = "valor";
+        public static final String COLUMN_NAME_TYPE = "tipo";
+        public static final String COLUMN_NAME_DATE = "data";
+    }
+}
---
+++ app/src/main/java/com/draft/rckt/equiperocket/Database/DatabaseHelper.java
@@ -0,0 +1,72 @@
+package com.draft.rckt.equiperocket.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Gabriel on 6/5/2016.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String REAL_TYPE = " REAL";
+    private static final String VARCHAR20_TYPE = " VARCHAR(20)";
+    private static final String VARCHAR40_TYPE = " VARCHAR(40)";
+    private static final String VARCHAR100_TYPE = " VARCHAR(100)";
+    private static final String VARCHAR255_TYPE = " VARCHAR(255)";
+    private static final String DATE_TYPE = " DATETIME";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + DatabaseContract.UserEntry.TABLE_NAME + " (" +
+                    DatabaseContract.UserEntry.COLUMN_NAME_USER_ID + " VARCHAR(40) PRIMARY KEY," +
+                    DatabaseContract.UserEntry.COLUMN_NAME_PASSWORD + VARCHAR20_TYPE + " NOT NULL" + COMMA_SEP +
+                    DatabaseContract.UserEntry.COLUMN_NAME_NAME + VARCHAR100_TYPE +
+                    " );"
+                    + "CREATE TABLE " + DatabaseContract.ReceitaEntry.TABLE_NAME + " (" +
+                    DatabaseContract.ReceitaEntry.COLUMN_NAME_ENTRY_ID + " INTEGER PRIMARY KEY," +
+                    DatabaseContract.ReceitaEntry.COLUMN_NAME_USER_ID + " VARCHAR(40) FOREIGN KEY REFERENCES "+
+                    DatabaseContract.UserEntry.TABLE_NAME+ "("+ DatabaseContract.UserEntry.COLUMN_NAME_USER_ID +")"+ " ON DELETE CASCADE"+COMMA_SEP +
+                    DatabaseContract.ReceitaEntry.COLUMN_NAME_TITLE + VARCHAR40_TYPE + COMMA_SEP +
+                    DatabaseContract.ReceitaEntry.COLUMN_NAME_CONTENT + VARCHAR255_TYPE+ COMMA_SEP +
+                    DatabaseContract.ReceitaEntry.COLUMN_NAME_VALUE + REAL_TYPE+ " NOT NULL" +COMMA_SEP +
+                    DatabaseContract.ReceitaEntry.COLUMN_NAME_TYPE + VARCHAR40_TYPE+ COMMA_SEP +
+                    DatabaseContract.ReceitaEntry.COLUMN_NAME_DATE + DATE_TYPE+ " NOT NULL DEFAULT NOW()"+
+                    " );"
+                    + "CREATE TABLE " + DatabaseContract.GastoEntry.TABLE_NAME + " (" +
+                    DatabaseContract.GastoEntry.COLUMN_NAME_ENTRY_ID + " INTEGER PRIMARY KEY," +
+                    DatabaseContract.GastoEntry.COLUMN_NAME_USER_ID + " VARCHAR(40) FOREIGN KEY REFERENCES "+
+                    DatabaseContract.UserEntry.TABLE_NAME+ "("+ DatabaseContract.UserEntry.COLUMN_NAME_USER_ID +")"+ " ON DELETE CASCADE"+COMMA_SEP +
+                    DatabaseContract.GastoEntry.COLUMN_NAME_TITLE + VARCHAR40_TYPE + COMMA_SEP +
+                    DatabaseContract.GastoEntry.COLUMN_NAME_CONTENT + VARCHAR255_TYPE+ COMMA_SEP +
+                    DatabaseContract.GastoEntry.COLUMN_NAME_VALUE + REAL_TYPE+ " NOT NULL" + COMMA_SEP +
+                    DatabaseContract.GastoEntry.COLUMN_NAME_TYPE + VARCHAR40_TYPE+ COMMA_SEP +
+                    DatabaseContract.GastoEntry.COLUMN_NAME_DATE + DATE_TYPE+ " NOT NULL DEFAULT NOW()"+
+                    " );";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + DatabaseContract.UserEntry.TABLE_NAME + ";"
+            +"DROP TABLE IF EXISTS " + DatabaseContract.ReceitaEntry.TABLE_NAME + ";"
+            +"DROP TABLE IF EXISTS " + DatabaseContract.GastoEntry.TABLE_NAME + ";";
+
+
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Database.db";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Conversao de versoes da database nao foi implementada
+        // Para nao causar inconsistencia, a atualizacao apaga a versao anterior
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
