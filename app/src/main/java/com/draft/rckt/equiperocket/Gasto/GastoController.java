d6d6f29b56a22937392191e93b103bccbbcdd216
cassuci <gabrielcassuci@gmail.com>
2016-06-09 16:49:40 -0300

    Criados pacotes e atividades de Gasto, Login e MainMenu

---
+++ app/src/main/java/com/draft/rckt/equiperocket/Gasto/GastoController.java
@@ -0,0 +1,15 @@
+package com.draft.rckt.equiperocket.Gasto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.draft.rckt.equiperocket.R;
+
+public class GastoController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gasto_controller);
+    }
+}
---
+++ app/src/main/java/com/draft/rckt/equiperocket/MainMenu/MainMenu.java
@@ -0,0 +1,16 @@
+package com.draft.rckt.equiperocket.MainMenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.draft.rckt.equiperocket.R;
+
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
