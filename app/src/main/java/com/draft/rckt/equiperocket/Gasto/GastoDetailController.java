11c6fd726ba40db5204a00f6c1116d119a77b73a
cassuci <gabrielcassuci@gmail.com>
2016-06-11 13:00:11 -0300

    Controladores de detalhamento de Gasto e Receita adicionados

---
+++ app/src/main/java/com/draft/rckt/equiperocket/Gasto/GastoDetailController.java
@@ -0,0 +1,15 @@
+package com.draft.rckt.equiperocket.Gasto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.draft.rckt.equiperocket.R;
+
+public class GastoDetailController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gasto_detail_controller);
+    }
+}
