e5b6ca967266fb08a2d44b6eb36a9f3933e8b1fb
cassuci <gabrielcassuci@gmail.com>
2016-06-11 13:02:49 -0300

    Controladores de Grafico e Relatorio adicionados

---
+++ app/src/main/java/com/draft/rckt/equiperocket/Grafico/GraficoController.java
@@ -0,0 +1,14 @@
+package com.draft.rckt.equiperocket.Grafico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.draft.rckt.equiperocket.R;
+
+public class GraficoController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grafico_controller);
+    }
+}
---
+++ app/src/main/java/com/draft/rckt/equiperocket/Relatorio/RelatorioController.java
@@ -0,0 +1,15 @@
+package com.draft.rckt.equiperocket.Relatorio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.draft.rckt.equiperocket.R;
+
+public class RelatorioController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorio_controller);
+    }
+}
