728d84b08e360f4154c2d1cdd1e5a85d3abbd69a
cassuci <gabrielcassuci@gmail.com>
2016-06-11 13:09:23 -0300

    Transferida responsabilidade de remoção de receita para o controlador de detalhes

--- app/src/main/java/com/draft/rckt/equiperocket/Receita/ReceitaController.java
+++ app/src/main/java/com/draft/rckt/equiperocket/Receita/ReceitaController.java
@@ -24,29 +24,4 @@ public class ReceitaController extends AppCompatActivity {
         setContentView(R.layout.activity_receita_controller);
     }
 
-    /**
-     * Remove registro da tabela de receitas
-     * @param receita_id identificador unico da receita
-     * @return true se um registro foi removido, false caso contrario
-     *
-     */
-    protected boolean removeReceita(int receita_id){
-
-        int n_rows; // numero de registros alterados na database
-
-        db = mDbHelper.getWritableDatabase(); // ganha acesso a database
-
-        // condicao de where da query
-        String where_clause = "WHERE " + DatabaseContract.ReceitaEntry.COLUMN_NAME_ENTRY_ID +
-                " = " + Integer.toString(receita_id);
-        n_rows = db.delete(DatabaseContract.ReceitaEntry.TABLE_NAME, where_clause,null);
-        db.close(); // libera database
-
-        // se alterou alguma linha retorna true
-        if (n_rows > 0)
-            return true;
-        else
-            return false;
-
-    }
 }
