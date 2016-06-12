b83ddc356e0d926b87ac0a096a30d7bd074b474b
cassuci <gabrielcassuci@gmail.com>
2016-06-12 13:23:31 -0300

    Receita detail controller complementado

--- app/src/main/java/com/draft/rckt/equiperocket/Receita/ReceitaDetailController.java
+++ app/src/main/java/com/draft/rckt/equiperocket/Receita/ReceitaDetailController.java
@@ -1,22 +1,31 @@
 package com.draft.rckt.equiperocket.Receita;
 
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.draft.rckt.equiperocket.Database.DatabaseContract;
 import com.draft.rckt.equiperocket.Database.DatabaseHelper;
+import com.draft.rckt.equiperocket.Gasto.CreateGastoDialog;
+import com.draft.rckt.equiperocket.Gasto.GastoController;
 import com.draft.rckt.equiperocket.R;
 
 import java.sql.Date;
 
-public class ReceitaDetailController extends AppCompatActivity implements View.OnClickListener {
+public class ReceitaDetailController extends AppCompatActivity implements View.OnClickListener, CreateReceitaDialog.NoticeDialogListener {
 
     private DatabaseHelper mDbHelper;
     private SQLiteDatabase db;
+
     private int receita_id;
     private String receita_title;
     private String receita_descr;
@@ -28,14 +37,77 @@ public class ReceitaDetailController extends AppCompatActivity implements View.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mDbHelper = new DatabaseHelper(this.getApplicationContext());
-        db = mDbHelper.getReadableDatabase(); // ganha acesso a database
+        mDbHelper = new DatabaseHelper(getApplicationContext());
 
-        //TODO:  implementar leitura dos detalhes da receita
+        // TODO: extrair detalhes da receita para mostrar na tela
 
-        db.close(); // libera database
+        setContentView(R.layout.activity_gasto_detail_controller);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        setContentView(R.layout.activity_receita_detail_controller);
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_detail_gasto_controller_menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(ReceitaDetailController.this, GastoController.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);// clear back stack
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.delete:
+                showNoticeDialog();
+                return true;
+            case R.id.edit:
+                Intent intent = new Intent(ReceitaDetailController.this, ReceitaModifyController.class);
+                intent.putExtra("receita_id", get_receita_id());
+                intent.putExtra("receita_title", get_receita_title());
+                intent.putExtra("receita_descr", get_receita_descr());
+                intent.putExtra("receita_value", get_receita_value());
+                intent.putExtra("receita_type", get_receita_type());
+                intent.putExtra("receita_date", get_receita_date());
+                startActivity(intent);
+                return true;
+
+        }
+        return true;
+    }
+
+    public void showNoticeDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new CreateGastoDialog();
+        dialog.show(getFragmentManager(), "NoticeDialogFragment");
+
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        dialog.dismiss();
+
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        boolean isDeleteSucessful =  removeReceita(get_receita_id());
+        dialog.dismiss();
+        if(isDeleteSucessful)
+            Toast.makeText(getApplicationContext(),"Gasto deletado com sucesso.",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(getApplicationContext(),"Falha ao deletar gasto." , Toast.LENGTH_SHORT).show();
     }
 
     @Override
