1d602a85ec89dc0236fa5a73bdfca040b53fff2e
Ivan Lucas Cabral <ivan.lucas010@gmail.com>
2016-06-15 22:26:54 -0300

    BD - datas arrumadas

--- app/src/main/java/com/draft/rckt/equiperocket/Database/DatabaseController.java
+++ app/src/main/java/com/draft/rckt/equiperocket/Database/DatabaseController.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.draft.rckt.equiperocket.Receita.Receita;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -39,6 +40,7 @@ public class DatabaseController {
                 contentValues.put("nome", "NomeTeste");
                 db.insert("Users", null, contentValues);
                 int i;
+
                 double v = 402;
                 for (i = 1; i < 15; i++) {
                     Receita rec = new Receita();
@@ -49,6 +51,7 @@ public class DatabaseController {
                     rec.tipo = "qualquer uma";
                     v += i;
                     rec.valor = v;
+                    rec.data = new Date();
                     addItemReceita(rec);
                 }
             }
@@ -56,20 +59,17 @@ public class DatabaseController {
     }
 
 
-
     public void addItemReceita(Receita rec) {
         db = dbHelper.getWritableDatabase();
 
         ContentValues contentValues = new ContentValues();
-
         contentValues.put(ReceitaEntry.COLUMN_NAME_ENTRY_ID, rec.receita_id);
         contentValues.put(ReceitaEntry.COLUMN_NAME_USER_ID, rec.user_id);
         contentValues.put(ReceitaEntry.COLUMN_NAME_TITLE, rec.titulo);
         contentValues.put(ReceitaEntry.COLUMN_NAME_CONTENT, rec.desc);
         contentValues.put(ReceitaEntry.COLUMN_NAME_VALUE, rec.valor);
         contentValues.put(ReceitaEntry.COLUMN_NAME_TYPE, rec.tipo);
-        //contentValues.put(ReceitaEntry.COLUMN_NAME_DATE, rec.data.getTime());
-
+        contentValues.put(ReceitaEntry.COLUMN_NAME_DATE, rec.data.getTime());
         db.insert(ReceitaEntry.TABLE_NAME, null, contentValues);
     }
 
@@ -77,8 +77,8 @@ public class DatabaseController {
 
         //TODO: depois arrumar o id para o id do usuário
         String query_get = "select * FROM " + ReceitaEntry.TABLE_NAME +
-                " WHERE " + ReceitaEntry.COLUMN_NAME_USER_ID + " = '1'";
-//                " ORDER BY " + ReceitaEntry.COLUMN_NAME_DATE + " DESC;";
+                " WHERE " + ReceitaEntry.COLUMN_NAME_USER_ID + " = '1'" +
+                " ORDER BY " + ReceitaEntry.COLUMN_NAME_DATE + " DESC;";
 
         // ganha acesso a database
         db = dbHelper.getReadableDatabase();
@@ -99,15 +99,13 @@ public class DatabaseController {
                 receita.user_id = cursor.getString(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_USER_ID));
                 receita.titulo = cursor.getString(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_TITLE));
                 receita.desc = cursor.getString(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_CONTENT));
-                receita.valor = cursor.getFloat(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_VALUE));
+                receita.valor = cursor.getDouble(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_VALUE));
                 receita.tipo = cursor.getString(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_TYPE));
-                //receita.data = new Date(cursor.getLong(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_DATE)));
-                //TODO arrumar data (IVAN)
+                receita.data = new Date(cursor.getLong(cursor.getColumnIndex(ReceitaEntry.COLUMN_NAME_DATE)));
                 array.add(receita);
 
             } while (cursor.moveToNext());
         }
-
         return array;
     }
 }
