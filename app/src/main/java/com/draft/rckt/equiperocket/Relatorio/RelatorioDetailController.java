15d558d9a50f087131e36608130ebf8233af99b6
ma-valverde <marina.valverde@gmail.com>
2016-06-19 16:03:07 -0300

    Modificações UI de Inserção de Gasto

--- app/src/main/java/com/draft/rckt/equiperocket/Relatorio/RelatorioDetailController.java
+++ app/src/main/java/com/draft/rckt/equiperocket/Relatorio/RelatorioDetailController.java
@@ -25,6 +25,14 @@ public class RelatorioDetailController extends AppCompatActivity {
     private TextView relatorioText;
     DatabaseController dbControl;
     RelatorioController rControl;
+    private String relatorio = "";
+    private int gastosSelected = RelatorioController.getGastosSelected(),
+            receitasSelected = RelatorioController.getReceitasSelected();
+    private float totalGastos = 0, totalReceitas = 0;
+    private Calendar startCal = RelatorioController.getStartCal(),
+            endCal = RelatorioController.getEndCal();
+    private Calendar startCalGasto = Calendar.getInstance(), endCalGasto = Calendar.getInstance(),
+            startCalRec = Calendar.getInstance(), endCalRec = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,68 +51,6 @@ public class RelatorioDetailController extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         relatorioText = (TextView) findViewById(R.id.relatorio_text);
-        montaRelatorio();
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-    }
-
-    private String relatorio = "";
-    private int gastosSelected = RelatorioController.getGastosSelected(),
-                receitasSelected = RelatorioController.getReceitasSelected();
-    private float totalGastos = 0, totalReceitas = 0;
-    private Calendar startCal = RelatorioController.getStartCal(),
-                     endCal = RelatorioController.getEndCal();
-
-    private void montaRelatorio()
-    {
-        cabecalho();
-        if(gastosSelected == 1)
-        {
-            relatorio += "Gastos:\n";
-            insereGastos();
-        }
-        if(receitasSelected == 1)
-        {
-            relatorio += "Receitas:\n";
-            insereReceitas();
-        }
-
-        if(gastosSelected == receitasSelected)
-            relatorio += "BALANÇO TOTAL = R$" + (totalReceitas - totalGastos) + "\n\n";
-
-        insereData();
-
-        relatorioText.setText(relatorio);
-    }
-
-    private void insereData() {
-        Date d = new Date();
-        SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
-        relatorio += "Relatório gerado em " + formatoData.format(d) + " às " +
-                DateFormat.getTimeInstance(DateFormat.SHORT).format(d);
-    }
-
-    private void insereGastos() {
-        // TODO
-        // obter gastos
-        //ArrayList<Gasto> list_gastos = DatabaseController.getGastoByPeriod(startCal, endCal);
-        //se tipo contido list_gastos.
-        relatorio += "\tNomeGasto1 = R$ 1.20\n\tNomeGasto2 = R$1000.00\n";
-        relatorio += "TOTAL GASTOS = R$" + totalGastos + "\n\n";
-    }
-
-    private void insereReceitas() {
-        // TODO
-        // obter receitas
-
-        ArrayList<Receita> allReceita = dbControl.getAllReceitaOrderByDate();
 
         // se user nao escolheu data inicial, ela eh a primeira do BD
         if (rControl.getStartYear() == 0)
@@ -118,14 +64,89 @@ public class RelatorioDetailController extends AppCompatActivity {
             endCal = null;
         }
 
+        montaRelatorio();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+    private void montaRelatorio()
+    {
+        String dados = "";
+
+        if(gastosSelected == 1)
+        {
+            dados += "Gastos:\n";
+            dados += insereGastos();
+        }
+        if(receitasSelected == 1)
+        {
+            dados += "Receitas:\n";
+            dados += insereReceitas();
+        }
+
+        if(gastosSelected == receitasSelected)
+            dados += "BALANÇO TOTAL = R$" + (totalReceitas - totalGastos) + "\n\n";
+
+        cabecalho();
+        relatorio += dados;
+        insereData();
+        relatorioText.setText(relatorio);
+    }
+
+    private void insereData() {
+        Date d = new Date();
+        SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
+        relatorio += "Relatório gerado em " + formatoData.format(d) + " às " +
+                DateFormat.getTimeInstance(DateFormat.SHORT).format(d);
+    }
+
+    private String insereGastos() {
+        ArrayList<Gasto> allGasto = dbControl.getAllGastoOrderByDate();
+        ArrayList<Gasto> gasto = dbControl.getGastoByPeriod(allGasto, startCal, endCal);
+        String relatorioRec = "";
+
+        for(int i = 0; i < gasto.size(); i++)
+        {
+            relatorioRec += String.format("\t%-30s%s%.2f\n", gasto.get(i).getTitulo(), "R$ ", gasto.get(i).getValor());
+            totalGastos += gasto.get(i).getValor();
+        }
+
+        relatorioRec += String.format("TOTAL GASTOS%22s%.2f\n\n", "R$ ", totalGastos);
+
+        if(startCal == null)
+            startCalGasto.setTime(gasto.get(gasto.size()-1).getData());
+        if(endCal == null)
+            endCalGasto.setTime(gasto.get(0).getData());
+
+        return relatorioRec;
+    }
+
+    private String insereReceitas() {
+        ArrayList<Receita> allReceita = dbControl.getAllReceitaOrderByDate();
         ArrayList<Receita> receita = dbControl.getReceitaByPeriod(allReceita, startCal, endCal);
+        String relatorioGasto = "";
+
         for(int i = 0; i < receita.size(); i++)
         {
-            relatorio += String.format("\t%-21s", receita.get(i).getTitulo()) + String.format("R$ %f", receita.get(i).getValor());
+            relatorioGasto += String.format("\t%-30s%s%.2f\n", receita.get(i).getTitulo(), "R$ ", receita.get(i).getValor());
             totalReceitas += receita.get(i).getValor();
         }
-        
-        relatorio += "TOTAL RECEITAS = R$" + totalReceitas + "\n\n";
+
+        relatorioGasto += String.format("TOTAL RECEITAS%20s%.2f\n\n", "R$ ", totalReceitas);
+
+        if(startCal == null)
+            startCalRec.setTime(receita.get(receita.size()-1).getData());
+        if(endCal == null)
+            endCalRec.setTime(receita.get(0).getData());
+
+        return relatorioGasto;
     }
 
     private void cabecalho() {
@@ -138,6 +159,21 @@ public class RelatorioDetailController extends AppCompatActivity {
 
         } else {
             relatorio = "RELATÓRIO DE RECEITAS\n\n";
+        }
+
+        if(startCal == null) {
+            // gasto < rec
+            if (startCalGasto.compareTo(startCalRec) < 0)
+                startCal = startCalGasto;
+            else
+                startCal = startCalRec;
+        }
+
+        if(endCal == null) {
+            if (endCalGasto.compareTo(endCalRec) < 0)
+                endCal = endCalGasto;
+            else
+                endCal = endCalRec;
         }
 
         relatorio += "Período: " +  rControl.formatDate(startCal) + " a " + rControl.formatDate(endCal) + "\n\n";
